c209c3609712cec408892471ecbe7a6004d239ef
abelantony <abelantony6@gmail.com>
2022-07-04 14:47:43 +0400

    New Project To Push GitHub

---
+++ example project/js/main.js
@@ -0,0 +1,25 @@
+
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
+
+
